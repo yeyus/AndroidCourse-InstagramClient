3e8bcea1ee8d382b9e1107f7ff63d546cbd32c49
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2015-01-24 20:09:16 -0800

    improving UI

--- app/src/main/java/com/ea7jmf/instagramviewer/InstagramPhoto.java
+++ app/src/main/java/com/ea7jmf/instagramviewer/InstagramPhoto.java
@@ -3,6 +3,8 @@ package com.ea7jmf.instagramviewer;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Date;
+
 /**
  * Created by jesusft on 1/22/15.
  */
@@ -13,13 +15,17 @@ public class InstagramPhoto {
     private String imageUrl;
     private int imageHeight;
     private int likesCount = 0;
+    private double latitude = 0.0;
+    private double longitude = 0.0;
+    private long createdTime;
 
-    public InstagramPhoto(String username, String caption, String imageUrl, int imageHeight, int likesCount) {
+    public InstagramPhoto(String username, String caption, String imageUrl, int imageHeight, int likesCount, long createdTime) {
         this.username = username;
         this.caption = caption;
         this.imageUrl = imageUrl;
         this.imageHeight = imageHeight;
         this.likesCount = likesCount;
+        this.createdTime = createdTime;
     }
 
     public String getUsername() {
@@ -62,13 +68,45 @@ public class InstagramPhoto {
         this.imageHeight = imageHeight;
     }
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(long createdTime) {
+        this.createdTime = createdTime;
+    }
+
     public static InstagramPhoto parse(JSONObject photoJSON) throws JSONException {
-        return new InstagramPhoto(
+        InstagramPhoto ip = new InstagramPhoto(
                 photoJSON.getJSONObject("user").getString("username"),
                 photoJSON.getJSONObject("caption").getString("text"),
                 photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url"),
                 photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height"),
-                photoJSON.getJSONObject("likes").getInt("count"));
+                photoJSON.getJSONObject("likes").getInt("count"),
+                Long.parseLong(photoJSON.getString("created_time")));
+
+        if (!photoJSON.isNull("location")) {
+            ip.setLatitude(photoJSON.getJSONObject("location").getDouble("latitude"));
+            ip.setLongitude(photoJSON.getJSONObject("location").getDouble("longitude"));
+        }
+
+        return ip;
     }
 
     @Override
@@ -77,7 +115,6 @@ public class InstagramPhoto {
                 "username='" + username + '\'' +
                 ", caption='" + caption + '\'' +
                 ", imageUrl='" + imageUrl + '\'' +
-                ", imageHeight=" + imageHeight +
                 ", likesCount=" + likesCount +
                 '}';
     }
--- app/src/main/java/com/ea7jmf/instagramviewer/InstagramPhotoAdapter.java
+++ app/src/main/java/com/ea7jmf/instagramviewer/InstagramPhotoAdapter.java
@@ -1,6 +1,7 @@
 package com.ea7jmf.instagramviewer;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +23,9 @@ import java.util.List;
 public class InstagramPhotoAdapter extends ArrayAdapter<InstagramPhoto> {
 
     TextView tvCaption;
+    TextView txtUsername;
+    TextView txtLocation;
+    TextView txtPostingTime;
     ImageView imgPhoto;
 
     public InstagramPhotoAdapter(Context context, List<InstagramPhoto> photos) {
@@ -38,9 +43,26 @@ public class InstagramPhotoAdapter extends ArrayAdapter<InstagramPhoto> {
         }
 
         tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        txtUsername = (TextView) convertView.findViewById(R.id.txtUsername);
+        txtLocation = (TextView) convertView.findViewById((R.id.txtLocation));
+        txtPostingTime = (TextView) convertView.findViewById(R.id.txtPostingTime);
         imgPhoto = (ImageView) convertView.findViewById(R.id.imgPhoto);
 
         tvCaption.setText(photo.getCaption());
+        txtUsername.setText(photo.getUsername());
+
+        // If there's any location information just show to the user
+        txtLocation.setVisibility(View.INVISIBLE);
+        if (photo.getLatitude() != 0.0 && photo.getLongitude() != 0.0) {
+            DecimalFormat df = new DecimalFormat("#.00");
+            txtLocation.setText("⚑ " + df.format(photo.getLatitude()) + ", " + df.format(photo.getLongitude()));
+            txtLocation.setVisibility(View.VISIBLE);
+        }
+
+        txtPostingTime.setText("🕛 " + DateUtils.getRelativeTimeSpanString(photo.getCreatedTime() * 1000,
+                System.currentTimeMillis(),
+                DateUtils.SECOND_IN_MILLIS));
+
         imgPhoto.getLayoutParams().height = photo.getImageHeight();
         // Reset image view
         imgPhoto.setImageResource(0);
